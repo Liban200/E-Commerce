01b16ae9218686562f27c0308308f191a934abb1
Liban200 <125518353+Liban200@users.noreply.github.com>
2023-07-19 22:45:00 -0400

    model folder code complete.

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,10 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
+const {NUMERIC} = require('sequelize');
+const {DECIMAL} = require('sequelize');
+
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,8 +14,43 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
   },
-  {
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 1,
+    validate: {
+      isNumeric: true,
+    },
+
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      },
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      },
+    },
+  },
+},
+
+{
     sequelize,
     timestamps: false,
     freezeTableName: true,
