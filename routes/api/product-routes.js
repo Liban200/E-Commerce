3c4c9f8f0b7a40bb559c196ad282ea782703785d
Liban200 <125518353+Liban200@users.noreply.github.com>
2023-07-20 17:25:53 -0400

    completed routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -25,7 +25,17 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create(req.body,{
+    
+      // All the fields you can update and the data attached to the request body.
+      products_name: req.body.products_name,
+      stock: req.body.stock,
+      category_id: req.body.category_id,
+      tagIds: [req.body.tagIds],
+      price: req.body.price,
+  
+    
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -89,8 +99,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productInfo = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productInfo) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(productInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
